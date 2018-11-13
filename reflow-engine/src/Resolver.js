a0144869046f5e89c2e5c11d5a0fed639742632a
adorsk <adorsk>
2018-11-13 11:02:22 -0500

    take out unused import loader

--- reflow-engine/src/Resolver.js
+++ reflow-engine/src/Resolver.js
@@ -1,23 +1,8 @@
 class Resolver {
   async resolve ({spec}) {
     if (spec.type === 'inline') { return spec.value }
-    if (spec.type === 'import') { return this.resolveImportSpec({spec}) }
     if (spec.type === 'fn') { return spec.fn() }
     throw new Error(`Could not resolve spec. spec was: '${spec}'`)
-  }
-
-  async resolveImportSpec ({spec}) {
-    const importSpecParts = spec.importSpec.split(':')
-    if (importSpecParts.length == 1) { importSpecParts.push(['default']) }
-    const [ moduleSpec, member ] = importSpecParts
-    const module = await import(moduleSpec)
-    if (!(member in module)) {
-      throw new Error(
-        'ImportError: could not find member '
-        + `'${member}' in module ${moduleSpec}.`
-      )
-    }
-    return module[targetMember]
   }
 }
 
